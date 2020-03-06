55d0daa1f63a2be254d91a933677b512d213b9a4
Josiah Maharaj <josiahmaharaj@outlook.com>
2020-03-06 15:52:22 -0400

    added advanced custom field

--- front-page.php
+++ front-page.php
@@ -4,19 +4,29 @@
   <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
 
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  
+      <?php
+      if (have_posts()) :
+        /* Start the Loop */
+        while (have_posts()) : the_post();
+      ?>
+      
+        
+        
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
       <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
       <li data-target="#myCarousel" data-slide-to="1"></li>
-      <!-- <li data-target="#myCarousel" data-slide-to="2"></li> -->
+      <li data-target="#myCarousel" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
+        <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg> -->
+        <img src="<?php the_field('image1'); ?>" class="d-block w-100" alt="carousel1">
         <div class="container">
           <div class="carousel-caption text-left">
-            <h1>Example headline.</h1>
-            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            <h1><?php the_field('headline1'); ?></h1>
+            <p><?php the_field('description1'); ?></p>
             <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
           </div>
         </div>
@@ -31,17 +41,6 @@
           </div>
         </div>
       </div>
-      <!-- <div class="carousel-item">
-        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
-        <div class="container">
-          <div class="carousel-caption text-right">
-            <h1>One more for good measure.</h1>
-            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
-          </div>
-        </div>
-      </div>
-    </div> -->
     <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
@@ -51,18 +50,13 @@
       <span class="sr-only">Next</span>
     </a>
   </div>
-
   <div class="container">
-      <?php
-      if (have_posts()) :
-        /* Start the Loop */
-        while (have_posts()) : the_post();
-      ?>
-      
-        <div>
+
+  <!-- <div>
           <a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
         </div>
-        
+  <p><?php the_content(); ?></p> -->
+
       <?php
         endwhile;
         /* End the Loop */
